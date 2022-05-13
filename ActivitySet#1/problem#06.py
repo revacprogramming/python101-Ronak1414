97bb0b8e11304355e76edc712e1f82080f09e36e
Ronak <96277743+Ronak1414@users.noreply.github.com>
2022-05-13 13:49:47 +0000

    Done!!

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,35 @@
 # Loops & Iterators
-
+'''
 largest = None
 smallest = None
 
 while True:
     num = input("Enter a number? ")
 
-    if num == "done":
-        break
+y        break
 
     # ...
 
     print(num)
 
 print("Maximum", largest)
+'''
+largest = -1
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try :
+        num=int(num)
+    except :
+        print("Invalid input")
+        continue
+    if   num > largest:
+        largest=num
+    if  smallest is None or num < smallest:
+           smallest=num
+   # print(num)
+
+print("Maximum", largest)
+print("Minimum",smallest)
--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,8 @@
 # Strings
 
 text = "X-DSPAM-Confidence:    0.8475"
+
+a=text.find("0")
+s=float()
+s=text[a:]
+print(s)
