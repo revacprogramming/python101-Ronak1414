0226abb888400ba04c2b1fe25e3114c14cbc88a0
Ronak <96277743+Ronak1414@users.noreply.github.com>
2022-05-15 11:00:52 +0000

    ....

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,5 +1,16 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
-file=filename.open()
+#filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+for line in fh:
+    if  line.startswith("X-DSPAM-Confidence:    0.8475"):
+        count=count+1
+        
+        continue
+    
+  
+avg=float(float(count)/float(line))
+print("Average spam confidence:",avg)
 
